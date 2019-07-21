f734c9147fca6b6e16198f6aa72955951c1109f0
hansonlam1 <hansonlam1@gmail.com>
2019-07-21 14:43:47 -0700

    stripping out a specific table

--- main.py
+++ main.py
@@ -3,20 +3,28 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-tickers = ["EMA","CNR"]
+tickers = ["EMA"]
+data = []
 
 for ticker in tickers:
     page_url = "http://ca.dividendinvestor.com/?symbol=" + ticker + "&submit=GO"
     print(page_url)
     page_response = requests.get(page_url, headers={'User-Agent':'Mozilla/5.0'},timeout = 5)
-    page_content = BeautifulSoup(page_response.content, "html.parser")
-    textContent = []
-    for i in range(0,30):
-        tablerows = page_content.find_all("tr")[i].text
-        textContent.append(tablerows)
+    page_content = BeautifulSoup(page_response.content)
+    
+    tbl = page_content.find_all("table")[15] #div data is in the 16th table tag
 
-    print(textContent)
+    #go through each row then column
+    rows = tbl.find_all("tr")  
+    for row in rows:
+        #get the columns in each row
+        cols = row.find_all("td")
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols if ele])
+
+    print(data)
     #need to add code here to strip out the key info
     time.sleep(1) #wait a second between requests
 
 #use the key info found
+
