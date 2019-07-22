e7d5e0c90a3f1aee3f523d701483cf008dcc9622
hansonlam1 <hansonlam1@gmail.com>
2019-07-21 22:03:18 -0700

    fix

--- main.py
+++ main.py
@@ -14,15 +14,15 @@ for ticker in tickers:
     page_url = "http://ca.dividendinvestor.com/?symbol=" + ticker + "&submit=GO"
     page_response = requests.get(page_url, headers={'User-Agent':'Mozilla/5.0'},timeout = 5)
     page_content = BeautifulSoup(page_response.content)
-    
     tbl = page_content.find_all("table")[15] #div data is in the 16th table tag
     rows = tbl.findChildren("tr")   #go through each row
 
     for row in rows:
         cell = row.find("td")
-        #if cell.text=="Dividend Pay Date:":
-        print(cell.string)
+        if cell.string=="Dividend Pay Date:":
+            print(cell.string)
 
     #main_dict.update(ticker_dict)   #add the ticker dictionary to the main one
     time.sleep(1) #wait a second between requests
+
 print("done")
