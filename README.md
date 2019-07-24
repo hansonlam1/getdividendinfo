e1ec1fb9c6947dd5d216673a2431d760a1a91e27
Hanson Lam <45834822+hansonlam1@users.noreply.github.com>
2019-07-23 21:57:11 -0700

    Development (#1)
    * stripping out a specific table
    
    * more progress
    
    * more progress
    
    * trying to match specific row
    
    * fix
    
    * fixed space after label
    
    * got the main script working
    
    * main functions working
    
    * updated readme
    
    * readme formatting
    
    * trygoogle
    
    * try google
    
    * separated tickerlist fixed date error
    
    * shorter timewait
    
    * updated readme

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # getdividendinfo
-Trying to get dividend information for Canadian stocks.
+
+# General Info:
+This project I'm trying to get dividend information for Canadian stocks.
 I want to know which stocks are paying dividends in the upcoming few days.
+
+# Usage:
+Update the tickers in the tickerlist.py file.
+Run the main.py script.
+
+# Future To Do:
+- parameterize some settings
+- import lists of tickers from various sources
+- handle none and not found rows
+- output to google sheets
+- automate the run
--- main.py
+++ main.py
@@ -2,21 +2,33 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+import pandas as pd
+from datetime import datetime, timedelta
+import tickerlist
 
-tickers = ["EMA","CNR"]
+maxdate = datetime.now() + timedelta(days=14)
+tickers = tickerlist.TICKERLIST
+main_df = pd.DataFrame()
 
 for ticker in tickers:
+    ticker_dict = {}    #we will append the values for each ticker to a dictionary
+    ticker_dict.clear() #clear it out for each ticker
+    ticker_dict = {"ticker" : ticker }
     page_url = "http://ca.dividendinvestor.com/?symbol=" + ticker + "&submit=GO"
-    print(page_url)
-    page_response = requests.get(page_url, headers={'User-Agent':'Mozilla/5.0'},timeout = 5)
-    page_content = BeautifulSoup(page_response.content, "html.parser")
-    textContent = []
-    for i in range(0,30):
-        tablerows = page_content.find_all("tr")[i].text
-        textContent.append(tablerows)
+    page_response = requests.get(page_url, headers={'User-Agent':'Mozilla/6.0'},timeout = 5)
+    page_content = BeautifulSoup(page_response.content)
+    tbl = page_content.find_all("table")[15] #div data is in the 16th table tag
+    rows = tbl.findChildren("tr")   #go through each row
+    for row in rows:
+        cells = row.findAll("td")
+        if len(cells) > 1:  #only want rows with two cells
+            ticker_dict.update({cells[0].text.strip():cells[1].text.strip()})
+    main_df = main_df.append(ticker_dict,ignore_index=True)
+time.sleep(0.3) #throttle it a bit
 
-    print(textContent)
-    #need to add code here to strip out the key info
-    time.sleep(1) #wait a second between requests
-
-#use the key info found
+main_df["Dividend Pay Date:"]=pd.to_datetime(main_df["Dividend Pay Date:"])
+main_df["Dividend Ex Date:"]=pd.to_datetime(main_df["Dividend Ex Date:"])
+main_df["Dividend Record Date:"]=pd.to_datetime(main_df["Dividend Record Date:"])
+#main_df = main_df[main_df["Dividend Pay Date:"]<=maxdate]
+main_df = main_df[(main_df["Dividend Pay Date:"]<=maxdate) & (main_df["Dividend Pay Date:"] >= datetime.now())]
+main_df[["ticker","Dividend Pay Date:"]]
